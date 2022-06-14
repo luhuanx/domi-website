a09049303d350a54b9766fb558d7d2a06d4290c1
Mimanchi <1640247539@qq.com>
2022-06-14 17:25:09 +0800

    deploy

---
+++ precache-manifest.16f4ee5b5c0a7112d6c23297996a5962.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c12870877cc9552df7a",
+    "url": "/domi-website/css/app.afcaf1b7.css"
+  },
+  {
+    "revision": "e5e8f32255ca735b9700",
+    "url": "/domi-website/css/chunk-vendors.9181e156.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/domi-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/domi-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "887d67b3cd0ba636f337205dc7853646",
+    "url": "/domi-website/img/record.887d67b3.png"
+  },
+  {
+    "revision": "d58ae1d5f5c7d4c629e92c8f6e8c03a8",
+    "url": "/domi-website/index.html"
+  },
+  {
+    "revision": "1c12870877cc9552df7a",
+    "url": "/domi-website/js/app.ded5d224.js"
+  },
+  {
+    "revision": "e5e8f32255ca735b9700",
+    "url": "/domi-website/js/chunk-vendors.97063495.js"
+  },
+  {
+    "revision": "425b5b167787750aa68f55e8db6ff727",
+    "url": "/domi-website/logo.png"
+  },
+  {
+    "revision": "894a1ac3affd984ba1939726fbda3c51",
+    "url": "/domi-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/domi-website/robots.txt"
+  }
+]);
